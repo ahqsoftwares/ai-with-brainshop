d4d9ac96774e0e5accd5cfed06b866c55be7a42b
AHQ Miness <ahqsecret@gmail.com>
2023-05-24 19:25:39 +0530

    some new data building

--- data.js
+++ data.js
@@ -1,8 +1,24 @@
 const s4d = "https://scratch-for-discord.com";
 
 module.exports = [
-    {input: "Hello", output: "Hi"},
-    {input: "Hi", output: "Hello! What can I do for you today!"},
-    {input: "What is the scratch for discord site?", output: s4d},
-    {input: "What is the s4d site?", output: s4d}
+    {
+        input: "What is the scratch for discord site?", 
+        output: s4d
+    },
+    {
+        input: "What is the s4d site?", 
+        output: s4d
+    },
+    {
+        input: "What is the full form of s4d?", 
+        output: "The full form is **scratch for discord**"
+    },
+    {
+        input: "Who made s4d?", 
+        output: "Androz"
+    },
+    {
+        input: "What is the difference between the link you sent and the https://scratch-for-discord.netlify.app ?", 
+        output: "The https://scratch-for-discord.netlify.app is the original s4d but it is now outdated and suited for use. Also, that link I sent has a lot of features compared to the main one"
+    }
 ]
